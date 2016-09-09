1aebe600f320cf00c9695a6c6e08c067d84b6d1c
tcabel <tristan.cabel@amadeus.com>
2016-09-09 11:26:51 +0200

    remove elpa from git

--- elpa/auto-complete-clang-20140409.52/auto-complete-clang-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; auto-complete-clang-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("auto-complete-clang.el") (21938 30540
-;;;;;;  11968 521000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; auto-complete-clang-autoloads.el ends here
